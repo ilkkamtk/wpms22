14109e2d1cbbd85896129f020d1b3cd14539b2ce
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2022-08-23 15:18:37 +0300

    components

--- components/List.js
+++ components/List.js
@@ -1,12 +1,99 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  Platform,
+  StyleSheet,
+} from 'react-native';
+
+const mediaArray = [
+  {
+    key: '0',
+    title: 'Title 1',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
+    thumbnails: {
+      w160: 'http://placekitten.com/160/161',
+    },
+    filename: 'http://placekitten.com/2048/1920',
+  },
+  {
+    key: '1',
+    title: 'Title 2',
+    description:
+      'Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ',
+    thumbnails: {
+      w160: 'http://placekitten.com/160/164',
+    },
+    filename: 'http://placekitten.com/2041/1922',
+  },
+  {
+    key: '2',
+    title: 'Title 3',
+    description:
+      'Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ',
+    thumbnails: {
+      w160: 'http://placekitten.com/160/167',
+    },
+    filename: 'http://placekitten.com/2039/1920',
+  },
+];
 
 const List = () => {
   return (
-    <View>
-      <Text>List</Text>
-    </View>
+    <FlatList
+      data={mediaArray}
+      renderItem={({item}) => {
+        return (
+          <TouchableOpacity style={styles.row}>
+            <View style={styles.box}>
+              <Image
+                style={styles.image}
+                source={{uri: item.thumbnails.w160}}
+              />
+            </View>
+            <View style={styles.box}>
+              <Text style={styles.listTitle}>{item.title}</Text>
+              <Text>{item.description}</Text>
+            </View>
+          </TouchableOpacity>
+        );
+      }}
+    />
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  droidSafeArea: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: Platform.OS === 'android' ? 30 : 0,
+  },
+  row: {
+    flexDirection: 'row',
+    backgroundColor: '#ccc',
+    marginBottom: 10,
+  },
+  box: {
+    flex: 1,
+    padding: 10,
+  },
+  image: {
+    flex: 1,
+  },
+  listTitle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    paddingBottom: 15,
+  },
+});
 
 export default List;
