f48768b1bb7dedd1026b13dfde9c04d7e18ce8b6
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2022-08-23 11:10:28 +0300

    first commit

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Moro si sano vorssalaine!</Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
