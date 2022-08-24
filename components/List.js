3fd72c4c3a6a597ab3dd79965f67c97cf04b372b
mattpe <mattpe@metropolia.fi>
2022-08-24 09:57:28 +0300

    add useEffect, http-a ready

--- components/List.js
+++ components/List.js
@@ -1,24 +1,30 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import {FlatList} from 'react-native';
 import ListItem from './ListItem';
 
 const List = () => {
   const url =
     'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
-
   const [mediaArray, setMediaArray] = useState([]);
-
   const loadMedia = async () => {
-    const response = await fetch(url);
-    const json = await response.json();
-    console.log(json);
-    setMediaArray(json);
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      console.log(json);
+      setMediaArray(json);
+    } catch (error) {
+      console.log('media fetch failed', error);
+      // TODO: notify user?
+    }
   };
-  //loadMedia();
+  useEffect(() => {
+    loadMedia();
+  }, []);
 
   return (
     <FlatList
       data={mediaArray}
+      keyExtractor={(item, index) => index.toString()}
       renderItem={({item}) => <ListItem singleMedia={item} />}
     />
   );
