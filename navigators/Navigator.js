9d94c77b211e3f8b382a3c9ffa30046212fe04a0
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2022-08-30 11:38:09 +0300

    navigation in progress

--- navigators/Navigator.js
+++ navigators/Navigator.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from '../views/Home';
 import Profile from '../views/Profile';
+import Single from '../views/Single';
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+const TabScreen = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
+  );
+};
+
+const StackScreen = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Tabs" component={TabScreen} />
+      <Stack.Screen name="Single" component={Single} />
+    </Stack.Navigator>
+  );
+};
 
 const Navigator = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Profile" component={Profile} />
-      </Tab.Navigator>
+      <StackScreen />
     </NavigationContainer>
   );
 };
