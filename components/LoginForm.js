156f7fd354971e518c40a5d303b40db56ad63bac
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2022-09-06 14:06:07 +0300

    esim puolesavälisä

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,7 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {Input, Button, Text} from '@rneui/themed';
 import {useContext} from 'react';
 import {Controller, useForm} from 'react-hook-form';
-import {View, Text, Button, TextInput} from 'react-native';
+import {View} from 'react-native';
 import {MainContext} from '../contexts/MainContext';
 import {useLogin} from '../hooks/ApiHooks';
 
@@ -39,35 +40,41 @@ const LoginForm = () => {
           minLength: 3,
         }}
         render={({field: {onChange, onBlur, value}}) => (
-          <TextInput
+          <Input
             onBlur={onBlur}
             onChangeText={onChange}
             value={value}
             placeholder="Username"
+            autoCapitalize="none"
+            errorMessage={
+              (errors.username?.type === 'required' && (
+                <Text>This is required.</Text>
+              )) ||
+              (errors.username?.type === 'minLength' && (
+                <Text>Min 3 chars!</Text>
+              ))
+            }
           />
         )}
         name="username"
       />
-      {errors.username?.type === 'required' && <Text>This is required.</Text>}
-      {errors.username?.type === 'minLength' && <Text>Min 3 chars!</Text>}
-
       <Controller
         control={control}
         rules={{
           required: true,
         }}
         render={({field: {onChange, onBlur, value}}) => (
-          <TextInput
+          <Input
             onBlur={onBlur}
             onChangeText={onChange}
             value={value}
             secureTextEntry={true}
             placeholder="Password"
+            errorMessage={errors.password && <Text>This is required.</Text>}
           />
         )}
         name="password"
       />
-      {errors.password && <Text>This is required.</Text>}
 
       <Button title="Sign in!" onPress={handleSubmit((data) => logIn(data))} />
     </View>
--- views/Profile.js
+++ views/Profile.js
@@ -3,7 +3,7 @@ import {MainContext} from '../contexts/MainContext';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {useTag} from '../hooks/ApiHooks';
 import {mediaUrl} from '../utils/variables';
-import {Button, Image, Text} from '@rneui/themed';
+import {Avatar, Button, Card, Icon, ListItem, Text} from '@rneui/themed';
 
 const Profile = () => {
   const {isLoggedIn, setIsLoggedIn, user} = useContext(MainContext);
@@ -37,18 +37,24 @@ const Profile = () => {
   };
 
   return (
-    <>
-      <Text>Profile</Text>
-      <Text>
-        User: {user.username} (id: {user.user_id})
-      </Text>
-      <Image source={{url: avatar}} style={{width: 200, height: 200}} />
-
-      <Text>Email: {user.email}</Text>
-      <Text>Full name: {user.full_name}</Text>
-      <Text>User since: {user.time_created}</Text>
+    <Card>
+      <Card.Title>{user.full_name}</Card.Title>
+      <Card.Image source={{uri: avatar}} />
+      <ListItem>
+        <Avatar
+          icon={{name: 'contact-mail', type: 'material'}}
+          containerStyle={{backgroundColor: '#aaa'}}
+        />
+        <Text>{user.email}</Text>
+      </ListItem>
+      <ListItem>
+        <Icon name="person" />
+        <Text>
+          {user.username} (id: {user.user_id})
+        </Text>
+      </ListItem>
       <Button title="Logout" onPress={logOut} />
-    </>
+    </Card>
   );
 };
 
