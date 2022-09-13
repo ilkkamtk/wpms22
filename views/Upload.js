ffcdd424735eecee4fcc6ef17e0093863746d025
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2022-09-13 10:18:26 +0300

    upload started

--- views/Upload.js
+++ views/Upload.js
@@ -1,11 +1,60 @@
-import {Text, View} from 'react-native';
-import React from 'react';
+import {Input, Button, Text, Card} from '@rneui/themed';
+import {Controller, useForm} from 'react-hook-form';
 
 const Upload = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm({
+    defaultValues: {title: '', description: ''},
+  });
+
+  const pickImage = () => {};
+
   return (
-    <View>
-      <Text>Upload</Text>
-    </View>
+    <Card>
+      <Card.Image source={{uri: 'https://placekitten.com/300'}} />
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+          minLength: 3,
+        }}
+        render={({field: {onChange, onBlur, value}}) => (
+          <Input
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            placeholder="Title"
+            autoCapitalize="words"
+            errorMessage={
+              (errors.title?.type === 'required' && (
+                <Text>This is required.</Text>
+              )) ||
+              (errors.title?.type === 'minLength' && <Text>Min 3 chars!</Text>)
+            }
+          />
+        )}
+        name="title"
+      />
+      <Controller
+        control={control}
+        render={({field: {onChange, onBlur, value}}) => (
+          <Input
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            placeholder="Description"
+          />
+        )}
+        name="description"
+      />
+
+      <Button title="Select media" onPress={pickImage} />
+
+      <Button title="Upload media" />
+    </Card>
   );
 };
 
