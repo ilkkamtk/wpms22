63f70ba318e795855239167d3bfa7314d66ee9bb
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2022-09-20 11:05:34 +0300

    orientation

--- views/Single.js
+++ views/Single.js
@@ -5,10 +5,25 @@ import {mediaUrl} from '../utils/variables';
 import {Card, ListItem, Text, Avatar} from '@rneui/themed';
 import FullSizeImage from '../components/FullSizeImage';
 import {Video} from 'expo-av';
+import {useEffect} from 'react';
+import * as ScreenOrientation from 'expo-screen-orientation';
+import {useState} from 'react';
 
 const Single = ({route}) => {
-  console.log('Single route', route);
+  // console.log('Single route', route);
   const {filename, title, description, user_id, media_type} = route.params;
+  const [videoRef, setVideoRef] = useState(null);
+
+  const handleVideoRef = (component) => {
+    setVideoRef(component);
+  };
+
+  useEffect(() => {
+    ScreenOrientation.addOrientationChangeListener((evt) => {
+      console.log('Orientaatio:', evt);
+    });
+  }, [videoRef]);
+
   return (
     <ScrollView>
       <Card>
@@ -21,7 +36,16 @@ const Single = ({route}) => {
             style={{marginBottom: 12}}
           />
         ) : (
-          <Video source={{uri: mediaUrl + filename}} />
+          <Video
+            ref={handleVideoRef}
+            source={{uri: mediaUrl + filename}}
+            style={{width: '100%', height: '100%'}}
+            onError={(error) => {
+              console.log('Video error:', error);
+            }}
+            useNativeControls
+            resizeMode="cover"
+          />
         )}
         <Card.Divider />
         <ListItem>
