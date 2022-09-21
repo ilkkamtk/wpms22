bdce0534eef2396fdcd40a98a03a23918c0ab65e
mattpe <mattpe@metropolia.fi>
2022-09-21 09:47:16 +0300

    player task b 3 done

--- hooks/ApiHooks.js
+++ hooks/ApiHooks.js
@@ -1,13 +1,18 @@
-import {useEffect, useState} from 'react';
+import {useContext, useEffect, useState} from 'react';
+import {MainContext} from '../contexts/MainContext';
 import {doFetch} from '../utils/http';
 import {apiUrl, applicationTag} from '../utils/variables';
 
-const useMedia = (update) => {
+const useMedia = (update, myFilesOnly = false) => {
   const [mediaArray, setMediaArray] = useState([]);
+  const {user} = useContext(MainContext);
   const loadMedia = async () => {
     try {
-      const json = await useTag().getFilesByTag(applicationTag);
-      // console.log(json);
+      let json = await useTag().getFilesByTag(applicationTag);
+      console.log(json);
+      if (myFilesOnly) {
+        json = json.filter((file) => file.user_id === user.user_id);
+      }
       const allMediaData = json.map(async (mediaItem) => {
         return await doFetch(apiUrl + 'media/' + mediaItem.file_id);
       });
